36f9cb1521316e29bb52b0a8381a7ce987645d16
adeptex <adeptex@users.noreply.github.com>
2018-05-01 01:59:08 +0200

    Create AESCipher.py

---
+++ AESCipher.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import base64
+
+from Crypto import Random
+from Crypto.Cipher import AES
+
+BS = 16
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+unpad = lambda s : s[0:-ord(s[-1])]
+
+
+class AESCipher:
+
+    def __init__( self, key ):
+        self.key = key
+
+    def encrypt( self, raw ):
+        raw = pad(raw)
+        iv = Random.new().read( AES.block_size )
+        cipher = AES.new( self.key, AES.MODE_CBC, iv )
+        return base64.b64encode( iv + cipher.encrypt( raw ) )
+
+    def decrypt( self, enc ):
+        enc = base64.b64decode(enc)
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv )
+        return unpad(cipher.decrypt( enc[16:] ))
+
+
+cipher = AESCipher('mysecretpassword')
+encrypted = cipher.encrypt('Secret Message A')
+decrypted = cipher.decrypt(encrypted)
+print encrypted
+print decrypted

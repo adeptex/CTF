9161bd1aeba3a37c9422a598a21cb56b7a5cb7a2
adeptex <adeptex@users.noreply.github.com>
2024-02-12 20:23:05 +0100

    Add ROT decoder

---
+++ rot.py
@@ -0,0 +1,21 @@
+# ROT13 or anything
+from sys import argv
+from string import ascii_lowercase, ascii_uppercase
+
+
+def rot(text, offset):
+    roted = ""
+
+    for ch in text:
+        if ch in ascii_lowercase:
+            roted += ascii_lowercase[(ord(ch) + offset) % 26]
+        elif ch in ascii_uppercase:
+            roted += ascii_uppercase[(ord(ch) + offset) % 26]
+        else:
+            roted += ch
+    
+    return roted
+
+
+for i in range(1, 26):
+    print(f"{i:02d}", rot(argv[1], i))
